9b869d14b7e2a6b4fc2f617c1515bee9380e37bb
Asher Carneiro <62783054+AsherCarneiro@users.noreply.github.com>
2021-07-01 18:04:13 +0530

    Add files via upload

---
+++ Day 4.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    // This program will create same sequence of
+    // random numbers on every program run
+ 
+    for(int i = 0; i<5; i++)
+        printf(" %d ", rand());
+   
+    return 0;
+    
+    // Day 4 Submission - Asher Carneiro
+}
